662b748a5b889b6114b019a84bde389d5937d406
azonto15 <jazzmtu4@gmail.com>
2023-07-25 09:39:32 -0700

    Updated

--- utils.c
+++ utils.c
@@ -10,36 +10,36 @@
 
 int is_printable(char c)
 {
-	if (c >= 32 && c < 127)
-		return (1);
+if (c >= 32 && c < 127)
+return (1);
 
 	return (0);
 }
 
 /**
- * append_hexa_code - append ascii in hexa code
- *
- * @buffer: an array of char
- * @i: index
- * @ascii_code: ASCII CODE
- *
- * Return: Always 0.
+* append_hexa_code - append ascii in hexa code
+*
+* @buffer: an array of char
+* @i: index
+* @ascii_code: ASCII CODE
+*
+* Return: Always 0.
 */
 
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	char map_to[] = "0123456789ABCDEF";
+char map_to[] = "0123456789ABCDEF";
 
-	if (ascii_code < 0)
-		ascii_code *= -1;
+if (ascii_code < 0)
+ascii_code *= -1;
 
-	buffer[i++] = '\\';
-	buffer[i++] = 'x';
+buffer[i++] = '\\';
+buffer[i++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+buffer[i++] = map_to[ascii_code / 16];
+buffer[i] = map_to[ascii_code % 16];
 
-	return (3);
+return (3);
 }
 
 /**
@@ -52,8 +52,8 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 int is_digit(char c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
+if (c >= '0' && c <= '9')
+return (1);
 
 	return (0);
 }
@@ -69,12 +69,12 @@ int is_digit(char c)
 
 long int convert_size_number(long int num, int size)
 {
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((short)num);
+if (size == S_LONG)
+return (num);
+else if (size == S_SHORT)
+return ((short)num);
 
-	return ((int)num);
+return ((int)num);
 }
 
 /**
@@ -88,10 +88,10 @@ long int convert_size_number(long int num, int size)
 
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((unsigned short)num);
+if (size == S_LONG)
+return (num);
+else if (size == S_SHORT)
+return ((unsigned short)num);
 
 	return ((unsigned int)num);
 }
